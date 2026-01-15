414328074e6569e30c59b335c7fcb88d8b1ad483
sagun <81767182+Saguny@users.noreply.github.com>
2026-01-15 11:31:44 +0100

    hand Rotation fix

--- MountainClimbTest/Assets/_Game/Scripts/Player/Quest3RotationFix.cs
+++ MountainClimbTest/Assets/_Game/Scripts/Player/Quest3RotationFix.cs
@@ -1,11 +1,22 @@
 using UnityEngine.XR.OpenXR.Features;
+
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEditor.XR.OpenXR.Features;
+#endif
 
+// The configuration attribute is only needed in the Editor to set up the build.
+// The runtime player doesn't need it.
+#if UNITY_EDITOR
 [OpenXRFeature(
-    TargetOpenXRApiVersion = "1.1.53", // This forces the correct loader version
-    UiName = "Quest 3 Rotation Fix (PCVR & Android)",
-    BuildTargetGroups = new[] { BuildTargetGroup.Standalone, BuildTargetGroup.Android }, // Standalone = PCVR
+    TargetOpenXRApiVersion = "1.1.53",
+    UiName = "Quest 3 Rotation Fix",
+    BuildTargetGroups = new[] { BuildTargetGroup.Standalone, BuildTargetGroup.Android },
     DocumentationLink = "https://issuetracker.unity3d.com/issues/xr-interaction-toolkit-xr-controllers-are-inverted-by-y-axis-when-using-meta-quest-3-with-openxr-plugin"
 )]
-public class FixQuest3Rotation : OpenXRFeature { }
+#endif
+public class Quest3RotationFix : OpenXRFeature
+{
+    // This class remains empty.
+    // It acts as a bridge to force the specific OpenXR version during the build process.
+}
